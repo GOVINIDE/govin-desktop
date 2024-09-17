2bf69a29d83caebc22e736b2813542c83f8fcaf7
GOVINIDE <idegovin@gmail.com>
2024-09-17 17:13:53 +0530

    first commit

---
+++ scripts/lib/libraries.js
@@ -0,0 +1,13 @@
+const backdrops = require('govin-gui/src/lib/libraries/backdrops.json');
+const costumes = require('govin-gui/src/lib/libraries/costumes.json');
+const sounds = require('govin-gui/src/lib/libraries/sounds.json');
+const sprites = require('govin-gui/src/lib/libraries/sprites.json');
+
+const libraries = {
+    backdrops,
+    costumes,
+    sounds,
+    sprites
+};
+
+module.exports = libraries;
